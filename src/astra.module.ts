6abee0d8621c52f3273a7695edae72a8d3011e3b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-04-26 18:36:13 +0200

    feat: Added various files

--- src/astra.module.ts
+++ src/astra.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AstraService } from './astra.service';
+import { DynamicModule, Module } from "@nestjs/common";
+import { AstraService } from "./astra.service";
+import { CONFIG_OPTIONS } from "./constants";
+import { AstraModuleConfig } from "./interfaces/astra-modue-config.interface";
 
-@Module({
-  providers: [AstraService]
-})
-export class AstraModule {}
+@Module({})
+export class AstraModule {
+  static forFeature(options: AstraModuleConfig): DynamicModule {
+    return {
+      module: AstraModule,
+      providers: [{ provide: CONFIG_OPTIONS, useValue: options }, AstraService],
+      exports: [AstraService],
+    };
+  }
+}
