15426cbc0a3ef11892ff696b8065f1734537a0d0
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-04-28 16:13:00 +0200

    fix: Import from right Module

--- src/astra.module.ts
+++ src/astra.module.ts
@@ -31,7 +31,7 @@ export class AstraModule {
   ): DynamicModule {
     return {
       module: AstraModule,
-      imports: [AstraModule.forRootAsync(options)],
+      imports: [AstraCoreModule.forRootAsync(options)],
     };
   }
 }
